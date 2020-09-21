5979870f6c73cdaaabd9371b95700ab59dc9b6b1
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2020-09-21 23:07:55 +0545

    header and home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Home from './Home';
+import Header from './Header';
 
 function App() {
   return (
     <div className="app">
       <h1>Let's Build AirBNB Clone</h1>
+      <Header />
+      <Home />
+
 
     </div>
   );
